414bea45962dcefbe217ac54ad21e9d7f87f7d1c
iolyab <iolyabru@gmail.com>
2025-02-26 21:59:31 -0500

    Added confirm password to signup page

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -9,11 +9,15 @@ const Signup = () => {
           <h2>Sign Up</h2>
           <div className={classes.email}>
             <p>Email</p>
-            <input type="email" placeholder="Enter your email..." className={classes.input} required />
+            <input type="email" placeholder="Enter email..." className={classes.input} required />
           </div>
           <div className={classes.password}>
             <p>Password</p>
-            <input type="password" placeholder="Create your password..." className={classes.input} required />
+            <input type="password" placeholder="Create password..." className={classes.input} required />
+          </div>
+          <div className={classes.confirmPassword}>
+            <p>Confirm Password</p>
+            <input type="password" placeholder="Confirm password..." className={classes.input} required />
           </div>
           <button type="submit" className={classes.btn}>Sign Up</button>
         </form>
