86ccd279b5ef145970430fe6e14dacb18a00dd92
iolyab <iolyabru@gmail.com>
2025-03-01 05:22:22 -0500

    Changed the navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,14 +4,32 @@ import classes from'./Nav.module.css';
 const Nav = () => {
     return(
         <nav className={classes.navbar}>
-            <p>Nav</p>
-            <ul>
-                <li>Search</li>
-                <li>Home</li>
-                <li>Dashboard</li>
-                <li>Calendar</li>
-            </ul>
+            <div className={classes.navContainer}>
+                    <button className={classes.btn}>
+                        <img className={classes.icon} src="/assets/search.png" alt="Search"/>
+                        Search
+                    </button>
+                <a href="/">
+                    <button className={classes.btn}>
+                        <img className={classes.icon} src="/assets/home.png" alt="Home"/>
+                        Home
+                    </button>
+                </a>
+                <button className={classes.btn}>
+                    <img className={classes.icon} src="/assets/dashboard.png" alt="Dashboard"/>
+                    Dashboard
+                </button>
+                <button className={classes.btn}>
+                    <img className={classes.icon} src="/assets/upcoming.png" alt="Upcoming"/>
+                    Upcoming
+                </button>
+                <button className={classes.btn}>
+                    <img className={classes.icon} src="/assets/calendar.png" alt="Calendar"/>
+                    Calendar
+                </button>
+            </div>
         </nav>
     )
 }
 export default Nav;
+
