a543d111ec6c6213708956fbb75199b5bca84de5
iolyab <iolyabru@gmail.com>
2025-04-28 17:23:09 -0400

    Changed date-fns to dayjs

--- src/pages/calendar/BigCalendar.jsx
+++ src/pages/calendar/BigCalendar.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 import { Layout } from "../../components/layout/Layout";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import dayjs from "dayjs";
+import weekday from "dayjs/plugin/weekday";
+import localeData from "dayjs/plugin/localeData";
+// import format from "date-fns/format";
+// import parse from "date-fns/parse";
+// import startOfWeek from "date-fns/startOfWeek";
+// import getDay from "date-fns/getDay";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useTasks } from "../../hooks/useTasks";
 
 const BigCalendar = () => {
-  const locales = {
-    "en-US": require("date-fns/locale/en-US"),
-  };
+  // const locales = {
+  //   "en-US": require("date-fns/locale/en-US"),
+  // };
 
-  const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-  });
+  // const localizer = dateFnsLocalizer({
+  //   format,
+  //   parse,
+  //   startOfWeek,
+  //   getDay,
+  //   locales,
+  // });
+
+  dayjs.extend(weekday);
+  dayjs.extend(localeData);
+
+  const localizer = dayjsLocalizer(dayjs);
 
   const { tasks } = useTasks();
 
   const events = tasks.map((task) => ({
     title: task.text,
-    start: new Date(task.deadline),
-    end: new Date(new Date(task.deadline).getTime() + 60 * 60 * 100),
+    start: dayjs(task.deadline).toDate(),
+    end: dayjs(task.deadline).add(1, "hour").toDate(),
   }));
 
   return (
