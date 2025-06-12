a2ef455e3ba30772cc032a5d84938dc43c51f91d
iolyab <iolyabru@gmail.com>
2025-06-12 11:29:23 -0400

    Added Material UI library, changed custom Input to Material UI input

--- src/components/shared/input/Input.jsx
+++ src/components/shared/input/Input.jsx
@@ -1,18 +1,42 @@
 import classes from "./input.module.scss";
 import PropTypes from "prop-types";
+import TextField from "@mui/material/TextField";
 
 const Input = ({ value, error, handleChange }) => {
   return (
-    <label className={classes.inputContainer}>
-      <input
-        type="text"
-        placeholder="To-do..."
+    <div className={classes.inputContainer}>
+      <TextField
+        label="To-do..."
+        variant="outlined"
         value={value}
         onChange={handleChange}
-        className={`${classes.input} ${error ? classes.error : ""}`}
-      ></input>
-      {error && <p className={classes.errorMessage}>{error}</p>}
-    </label>
+        error={Boolean(error)}
+        helperText={error}
+        fullWidth
+        size="small"
+        sx={{
+          "& .MuiOutlinedInput-input": {
+            padding: "10px 390px 10px 10px",
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#ccc",
+          },
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#48293d",
+          },
+          "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+            {
+              borderColor: "#48293d",
+            },
+          "& .MuiInputLabel-root": {
+            color: "#48293d",
+          },
+          "& .MuiInputLabel-root.Mui-focused": {
+            color: "#48293d",
+          },
+        }}
+      ></TextField>
+    </div>
   );
 };
 
